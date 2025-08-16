601017f9513dfaf53ae2c9fcb3ec93fc9f57dbf7
ayushjain3577 <ayushjain26589@gmail.com>
2025-08-16 17:12:28 +0530

    Time: 73 ms (5.28%) | Memory: 47.2 MB (5.92%) - LeetSync

---
+++ 297-serialize-and-deserialize-binary-tree/serialize-and-deserialize-binary-tree.java
@@ -0,0 +1,40 @@
+public class Codec {
+    public String recserialize(TreeNode root, String str){
+    
+        if(root == null){
+            str += "null,";
+        }
+        else{
+            str += str.valueOf(root.val) +",";
+            str = recserialize(root.left, str);
+            str = recserialize(root.right, str);
+        }
+        
+        return str;
+    }
+    public String serialize(TreeNode root) {
+        
+        return recserialize(root, "");
+        
+        
+    }
+    // Decodes your encoded data to tree.
+    public TreeNode recdeserialize(List<String> strs){
+        if(strs.get(0).equals("null")){
+            strs.remove(0);
+            return null;
+        }
+        TreeNode root = new TreeNode(Integer.valueOf(strs.get(0)));
+        strs.remove(0);
+        root.left= recdeserialize(strs);
+        root.right = recdeserialize(strs);
+        
+        return root;
+    }
+    public TreeNode deserialize(String data) {
+        
+        String[] strArray = data.split(",");
+        List<String> strList = new LinkedList<String>(Arrays.asList(strArray));
+        return recdeserialize(strList);
+    }
+}
